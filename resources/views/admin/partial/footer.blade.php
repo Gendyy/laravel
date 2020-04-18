7f3b06ed8a4ee517f89810aaf6ebfc0465f97d38
Gendyz <osamahmuhammed@gmail.com>
2020-04-18 19:52:17 +0200

    Admin Dashboard + User Mangement System

--- resources/views/admin/partial/footer.blade.php
+++ resources/views/admin/partial/footer.blade.php
@@ -212,26 +212,7 @@
 </script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{ url('')}}/design/adminlte/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- Morris.js charts -->
-{{-- <script src="{{ url('')}}/design/adminlte/bower_components/raphael/raphael.min.js"></script>
-<script src="{{ url('')}}/design/adminlte/bower_components/morris.js/morris.min.js"></script> --}}
-<!-- Sparkline -->
-{{-- <script src="{{ url('')}}/design/adminlte/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
-<!-- jvectormap -->
-<script src="{{ url('')}}/design/adminlte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-<script src="{{ url('')}}/design/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="{{ url('')}}/design/adminlte/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="{{ url('')}}/design/adminlte/bower_components/moment/min/moment.min.js"></script>
-<script src="{{ url('')}}/design/adminlte/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
-<!-- datepicker -->
-<script src="{{ url('')}}/design/adminlte/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
-<!-- Bootstrap WYSIHTML5 -->
-<script src="{{ url('')}}/design/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-<!-- Slimscroll -->
-<script src="{{ url('')}}/design/adminlte/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
+
 <script src="{{ url('')}}/design/adminlte/bower_components/fastclick/lib/fastclick.js"></script> --}}
 <!-- AdminLTE App -->
 <script src="{{ url('')}}/design/adminlte/dist/js/adminlte.min.js"></script>
